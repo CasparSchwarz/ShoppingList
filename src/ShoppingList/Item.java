34cf1cbe8b4f5eb315d140e344bcadfe882aca74
Caspar Schwarz <caspar.s@gmx.de>
2018-10-18 19:13:56 +0200

    Implement Item.java class

--- src/ShoppingList/Item.java
+++ src/ShoppingList/Item.java
@@ -4,11 +4,25 @@
  * and open the template in the editor.
  */
 package ShoppingList;
-
 /**
  *
  * @author smg
  */
 public class Item {
+    private String name;
+    private Category category;
+    private Store store;
+    private int priority;
+    private boolean check;
     
+    public Item (String name, Category category, int priority){
+        name = this.name;
+        category = this.category;
+        priority = this.priority;
+        check = false;
+    }
+    
+    public void check(){
+        check = true;
+    }
 }
