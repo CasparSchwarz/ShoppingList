c694478193030cdbbddd2bb0a7cb689959bd509e
smg <smg@smgimage>
2018-10-19 12:30:08 +0200

    Implementing amount in Item

--- src/ShoppingList/Item.java
+++ src/ShoppingList/Item.java
@@ -13,16 +13,22 @@ public class Item {
     private Category category;
     private Store store;
     private int priority;
+    private String amount;
     private boolean check;
     
-    public Item (String name, Category category, int priority){
+    public Item (String name, Category category, int priority, String amount){
         name = this.name;
         category = this.category;
         priority = this.priority;
+        amount = this.amount;
         check = false;
     }
     
     public void check(){
         check = true;
+    }
+    
+    public void changeAmount(String amount){
+        amount = this.amount;
     }
 }
