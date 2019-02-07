aa957b1ed80c0fbe3a466497170320b2f80628ca
smg <smg@smgimage>
2019-02-07 07:54:37 +0100

    +add onCreate method to connect Service

--- src/services/Service.java
+++ src/services/Service.java
@@ -14,6 +14,7 @@ public class Service {
     public void connect() throws SQLException{
         Connection conn = DriverManager.getConnection(url);
         dh = new DatabaseHelper(conn);
+        dh.onCreate();
     }
     
     public void addSL(String name){
