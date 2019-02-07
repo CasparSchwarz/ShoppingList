76d531aedfcaefb40c7c0e4158ade302d507454a
smg <smg@smgimage>
2019-02-07 09:11:29 +0100

    + LevenshteinDistance added (Organize)

--- src/services/Organize.java
+++ src/services/Organize.java
@@ -48,33 +48,46 @@ private static final int SORT_BY_CATEGORY = 1;
         Collections.sort(items, new Comparator<Item>() {
             @Override
             public int compare(Item o1, Item o2) {
-                if (o1.getPriority() == o2.getPriority()) {
-                    return 0;
-                } else if (o1.getPriority()) {
-                    return -1;
-                } else if (o2.getPriority()) {
-                    return 1;
-                } else {
-                    return 0;
-                }
+                return o1.getPriority() - o2.getPriority();
             }
         });
 
         Collections.sort(items, new Comparator<Item>() {
             @Override
             public int compare(Item o1, Item o2) {
-                if (o1.isCheck() == o2.isCheck()) {
-                    return 0;
-                } else if (o1.isCheck()) {
-                    return 1;
-                } else if (o2.isCheck()) {
-                    return -1;
-                } else {
-                    return 0;
-                }
+                return o1.getCheck() - o2.getCheck();
             }
         });
 
        
     }
-}
+    
+    public static void filter(String rein){
+        
+    }
+                                             
+    private static int minimum(int a, int b, int c) {                            
+            return Math.min(Math.min(a, b), c);                                      
+        }                                                                            
+
+        public static int computeLevenshteinDistance(CharSequence lhs, CharSequence rhs) {      
+            int[][] distance = new int[lhs.length() + 1][rhs.length() + 1];        
+
+            for (int i = 0; i <= lhs.length(); i++)                                 
+                distance[i][0] = i;                                                  
+            for (int j = 1; j <= rhs.length(); j++)                                 
+                distance[0][j] = j;                                                  
+
+            for (int i = 1; i <= lhs.length(); i++)                                 
+                for (int j = 1; j <= rhs.length(); j++)                             
+                    distance[i][j] = minimum(                                        
+                            distance[i - 1][j] + 1,                                  
+                            distance[i][j - 1] + 1,                                  
+                            distance[i - 1][j - 1] + ((lhs.charAt(i - 1) == rhs.charAt(j - 1)) ? 0 : 1));
+
+            return distance[lhs.length()][rhs.length()];                           
+        }                                                                            
+    }
+    
+    
+
